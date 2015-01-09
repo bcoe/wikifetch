7f86f658b55e31a8a992f87a15f565e7b867800b
bcoe <bencoe@gmail.com>
2015-01-08 22:07:13 -0800

    switched to using cheerio from jQuery.

--- bin/wikifetch.js
+++ bin/wikifetch.js
@@ -7,9 +7,9 @@ if (!argv.article) {
 	console.log('usage: wikifetch --article=[article_name]')
 } else {
 	var article = argv.article;
-	
+
 	console.log('Fetching article ' + argv.article + '.');
-	
+
 	var articleObject = (new WikiFetch()).fetch(article, function(err, articleObject) {
 		if (err) {
 			console.log('Could not fetch article: ' + err);
